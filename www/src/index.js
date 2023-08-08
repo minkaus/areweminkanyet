edd01e1ebd00762a1163064684fb167ad22f1953
Lukas Schulte Pelkum <lockas.sp@proton.me>
2023-08-08 16:19:38 +0200

    Update index.js

--- www/src/index.js
+++ www/src/index.js
@@ -1,13 +1,13 @@
 import * as aplang from "areweaplangyet-wasm";
 
-const REPO = "APLanguage/aplang-rs";
-const START_TIME = 1613750769000.0;
+const REPO = "minkan-chat/server";
+const START_TIME = 1626110100000.0;
 
 function getPhrase(days) {
   if (days == 0) {
-    return "A good day to be AP";
+    return "A good day for privacy-respecting messaging";
   } else {
-    return "Get off Discord";
+    return "Minkaus. Get off real life";
   }
 }
 
@@ -17,7 +17,7 @@ async function renderProgress() {
   const commit = `https://github.com/${REPO}/commit/${res.sha}`;
   const phrase = getPhrase(res.days);
 
-  const fmt = `${res.daysTotal} days without APLang, ${res.days} days since the last \
+  const fmt = `${res.daysTotal} days without Minkan, ${res.days} days since the last \
     <a href="${commit}">commit</a>. ${phrase}!`;
   document.getElementById("aplang-info").innerHTML = fmt;
 }
